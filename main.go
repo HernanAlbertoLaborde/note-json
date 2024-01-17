b7c19bec0526c422eb95cc5e0b8b0a35293f846d
Hernán A. Laborde <hernanlaborde@gmail.com>
2024-01-17 13:57:42 -0500

    :sparkles: Final commit

--- main.go
+++ main.go
@@ -14,7 +14,25 @@ type saver interface {
 	Save() error // Have a method Save and returns an error.
 }
 
+// type displayer interface {
+// 	Display()
+// }
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
+type outputtable interface {
+	saver
+	Display()
+}
+
 func main() {
+
+	printSomething(100)
+	printSomething(1.618)
+	printSomething("Oli")
 
 	title, content := getNoteData()
 	todoText := getUserInput("Todo text:")
@@ -26,26 +44,53 @@ func main() {
 	}
 
 	userNote, err := note.New(title, content)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	todo.Display()
-
-	err = saveData(todo)
-	if err != nil {
-
-	}
-
-	userNote.Display()
-
-	err = saveData(userNote)
+	err = outputData(todo)
 	if err != nil {
 		return
 	}
 
+	outputData(userNote)
+
+}
+
+func printSomething(value interface{}) {
+	// using interface{} allows this function to receive any value of any type.
+
+	intVal, ok := value.(int)
+	if ok {
+		fmt.Println("Integer: ", intVal)
+	}
+
+	floatVal, ok := value.(float64)
+	if ok {
+		fmt.Println("Float: ", floatVal)
+	}
+
+	stringVal, ok := value.(string)
+	if ok {
+		fmt.Println("String: ", stringVal)
+	}
+
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer: ", value)
+	// case float64:
+	// 	fmt.Println("Float: ", value)
+	// case string:
+	// 	fmt.Println("String: ", value)
+	// }
+
+}
+
+func outputData(data outputtable) error {
+	//This function Display the data in shell and also writes it in file.
+	data.Display()
+	return saveData(data)
 }
 
 func saveData(data saver) error {
